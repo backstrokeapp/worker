c11517f615b8d360800c3578019ef890a733bfd0
Ryan Gaus <rsg1egoman@gmail.com>
2017-08-15 20:22:54 -0400

    code: refactor into a package that will be installed by the main server

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const redis = require('redis').createClient(process.env.REDIS_URL);
 const RedisMQ = require('rsmq');
-const redisQueue = new RedisMQ({
-  client: redis,
-  ns: 'rsmq',
-});
+const redis = require('redis').createClient(process.env.REDIS_URL);
+const redisQueue = new RedisMQ({client: redis, ns: 'rsmq'});
 
-const OK = 'OK', RUNNING = 'RUNNING', ERROR = 'ERROR';
+// Perform a webhook update.
+const processBatch = require('./worker');
+
+
 const ONE_HOUR_IN_SECONDS = 60 * 60;
 const WebhookStatusStore = {
   set(webhookId, status, expiresIn=ONE_HOUR_IN_SECONDS) {
@@ -64,71 +64,43 @@ const WebhookQueue = {
   },
   pop() {
     return new Promise((resolve, reject) => {
-      redisQueue.popMessage({qname: this.queueName}, (err, {message, id}) => {
+      redisQueue.popMessage({qname: this.queueName}, (err, data) => {
         if (err) {
           reject(err);
-        } else if (typeof id === 'undefined') {
+        } else if (!data || typeof data.id === 'undefined') {
           // No items in the queue
           resolve(null);
         } else {
           // Item was found on the end of the queue!
-          resolve({data: message, id});
+          resolve({data: JSON.parse(data.message), id: data.id});
         }
       });
     });
-  }
+  },
 };
 WebhookQueue.initialize();
 
+
 // Logging function to use in webhooks.
-function logger() { console.log.apply(console, ['   *', ...arguments]); }
-
-// The batch processing function. Eats off the queue and publishes results to redis.
-async function processBatch(WebhookQueue, WebhookStatusStore) {
-  while (true) {
-    // Fetch a new webhook event.
-    const webhook = await WebhookQueue.pop();
-    // The queue is empty? Cool, we're done.
-    if (!webhook) { break; }
-
-    // Let redis know that we are starting to process this webhook.
-    const startedAt = (new Date()).toISOString();
-    await WebhookStatusStore.set(webhook.id, {
-      status: RUNNING,
-      startedAt,
-    });
-
-    // Extract a number of helpful values for use in the below code.
-    const content = JSON.parse(webhook.data);
-    const link = content.link;
-    const user = content.user;
-
-    // Log the type of update that is happening.
-    console.log(`=> * Handling webhook ${webhook.id}:`);
-    logger(`From: ${link.upstreamOwner}/${link.upstreamRepo}@${link.upstreamBranch}`);
-    if (link.forkType === 'fork-all') {
-      logger(`To: all forks @ ${link.upstreamBranch} (uses upstream branch)`);
-    } else {
-      logger(`To: ${link.forkOwner}/${link.forkRepo}@${link.forkBranch}`);
-    }
-
-    /* TODO: do the thing! :) */
-    const output = {foo: 'bar'};
-
-    // Successful! Update redis to say so.
-    await WebhookStatusStore.set(webhook.id, {
-      status: OK,
-      startedAt,
-      finishedAt: (new Date()).toISOString(),
-      output,
-    });
-  }
+function logger() {
+  console.log.apply(console, ['   *', ...arguments]);
 }
 
-// Every once and a while, process a new batch of webhook events
-const UPDATE_SECONDS = 30;
+
+
 if (require.main === module) {
-  processBatch(WebhookQueue, WebhookStatusStore).then(() => {
-    setInterval(() => processBatch(WebhookQueue, WebhookStatusStore), UPDATE_SECONDS * 1000);
+  // Called once the process finishes.
+  function final() {
+    redis.quit();
+  }
+
+  // Kick off the batch!
+  processBatch(WebhookQueue, WebhookStatusStore, logger).then(() => {
+    console.log('* Success!');
+    final();
+  }).catch(err => {
+    console.error('Error:');
+    console.error(err.stack);
+    final();
   });
 }
